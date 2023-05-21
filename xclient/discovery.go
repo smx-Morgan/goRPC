588bfb558714c77a38dd1cb9a428898081444abc
smx_Moragn <1048492656@qq.com>
2023-05-21 13:18:05 +0800

    实现服务器的负载均衡

---
+++ xclient/discovery.go
@@ -0,0 +1,80 @@
+package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect     SelectMode = iota // select randomly
+	RoundRobinSelect                   // select using Robbin algorithm
+)
+
+type Discovery interface {
+	Refresh() error                      // refresh from remote registry从注册中心更新服务列表
+	Update(servers []string) error       //手动更新服务列表
+	Get(mode SelectMode) (string, error) //根据均衡负载策略选择服务实列。
+	GetAll() ([]string, error)           // 返回所有服务实列
+}
+
+// 不需要注册中心，服务列表由手工维护的服务发现的结构体
+type MultiServersDiscovery struct {
+	r       *rand.Rand   // generate random number
+	mu      sync.RWMutex // protect following
+	servers []string
+	index   int // record the selected position for robin algorithm
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1) //index 是记录已经轮询到的位置
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n] // servers could be updated, so mode n to ensure safety
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers) //返回地址
+	return servers, nil
+}
